b61db16b34c7c296dd5a7481ca796df366f0fef8
Thomas Ferré <t.ferre@digitsole.com>
2022-06-24 11:49:44 +0200

    [#Model] Create sample datas
    - Add sample currencies datas for test

---
+++ MonBaluchon/MonBaluchon/Model/Currencies.Sample.swift
@@ -0,0 +1,17 @@
+//
+//  Currencies.Sample.swift
+//  MonBaluchon
+//
+//  Created by TomF on 24/06/2022.
+//
+
+import Foundation
+
+struct Currencies {
+    let currency = [
+        "United States Doller",
+        "Euro",
+        "Yen",
+        "Pound sterling"
+    ]
+}
