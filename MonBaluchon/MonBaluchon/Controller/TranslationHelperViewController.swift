0e1be2c1c92124bcd0c945939997c1b787038dda
Thomas Ferré <t.ferre@digitsole.com>
2022-06-28 17:26:17 +0200

    [#View] Translation Helper View
    - Create TranslationHelperViewController
    - Add IBOutlets on TranslationHelperViewController
    - Translation Helper View on storyboard

---
+++ MonBaluchon/MonBaluchon/Controller/TranslationHelperViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TranslationHelperViewController.swift
+//  MonBaluchon
+//
+//  Created by TomF on 28/06/2022.
+//
+
+import UIKit
+
+class TranslationHelperViewController: UIViewController {
+
+    @IBOutlet weak var firstLanguagePicker: UIButton!
+    @IBOutlet weak var secondLanguagePicker: UIButton!
+    @IBOutlet weak var textfieldForTranslate: UITextView!
+    @IBOutlet weak var translatedTextfield: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
