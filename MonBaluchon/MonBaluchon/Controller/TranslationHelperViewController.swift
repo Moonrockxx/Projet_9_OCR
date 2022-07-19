778c91ff77ccc71474130d417eb492da42b320c5
Thomas Ferré <t.ferre@digitsole.com>
2022-07-19 18:23:03 +0200

    [#ViewController] TranslationHelperViewController
    - Complience to UITextViewDelegate
    - Call to getLanguages on viewDidLoad
    - Add method textViewDidBeginEditing to erase placeholder when edition begins
    - Add createFilteringMenu method

--- MonBaluchon/MonBaluchon/Controller/TranslationHelperViewController.swift
+++ MonBaluchon/MonBaluchon/Controller/TranslationHelperViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class TranslationHelperViewController: UIViewController {
+class TranslationHelperViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var firstLanguagePicker: UIButton!
     @IBOutlet weak var secondLanguagePicker: UIButton!
     @IBOutlet weak var textfieldForTranslate: UITextView!
     @IBOutlet weak var translatedTextfield: UITextView!
+    @IBOutlet weak var getTranslationButton: UIButton!
     
     private var allElements: [UIView] = []
+    private var menuLanguages: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpView()
+        textfieldForTranslate.delegate = self
+        
+        LanguagesService.shared.getLanguages { [weak self] success, languages in
+            guard let languages = languages else {
+                return
+            }
+            
+            languages.lang?.forEach { key, value in
+                self?.menuLanguages.append(String(describing: value))
+            }
+            
+            self?.firstLanguagePicker.menu = self?.createFilteringMenu()
+            self?.secondLanguagePicker.menu = self?.createFilteringMenu()
+        }
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textfieldForTranslate.resignFirstResponder()
         translatedTextfield.resignFirstResponder()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textfieldForTranslate.text = String()
     }
     
     private func groupAllElements() {
@@ -41,5 +61,20 @@ class TranslationHelperViewController: UIViewController {
         
         textfieldForTranslate.contentInset.left = 8
         translatedTextfield.contentInset.left = 8
+    }
+    
+    private func createFilteringMenu() -> UIMenu {
+        var menuActions: [UIAction] = []
+        let optionsClosure = { (action: UIAction) in
+            print(action.title)
+        }
+        
+        for key in menuLanguages {
+            let item = UIAction(title: "\(value(forKey: key) ?? "")", handler: optionsClosure)
+            menuActions.append(item)
+            print(value(forKey: key) ?? "")
+        }
+        
+        return UIMenu(title: "Select a currency", children: menuActions)
     }
 }
