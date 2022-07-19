087e707f9104acfcec9dbdcbb2d3c24f3a9d479a
Thomas Ferré <t.ferre@digitsole.com>
2022-07-19 11:57:13 +0200

    [#Service] Apply singleton pattern
    - Add shared static property
    - Turn default init to private
    - Add private property task
    - Call to cancel before new task

--- MonBaluchon/MonBaluchon/Model/Services/CurrenciesService.swift
+++ MonBaluchon/MonBaluchon/Model/Services/CurrenciesService.swift
@@ -8,36 +8,44 @@
 import Foundation
 
 class CurrenciesService {
+    static var shared = CurrenciesService()
+    private init() {}
+    
     private static let symbolsUrl = URL(string: "https://api.apilayer.com/fixer/symbols?apikey=TeOLN3KvPxA1VOD4az4SXQLi7ac7RE71")!
     
-    static func getSymbols(callback: @escaping (Bool, Symbols?) -> Void) {
-        var request = URLRequest(url: symbolsUrl)
+    private var task: URLSessionDataTask?
+    
+    func getSymbols(callback: @escaping (Bool, Symbols?) -> Void) {
+        var request = URLRequest(url: CurrenciesService.symbolsUrl)
         request.httpMethod = "GET"
         
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                callback(false, nil)
-                return
+        task?.cancel()
+        task = session.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+                
+                guard let responseJSON = try? JSONDecoder().decode(Symbols.self, from: data) else {
+                    callback(false, nil)
+                    return
+                }
+                
+                let symbols = Symbols(symbols: responseJSON.symbols)
+                callback(true, symbols)
             }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                callback(false, nil)
-                return
-            }
-            
-            guard let responseJSON = try? JSONDecoder().decode(Symbols.self, from: data) else {
-                callback(false, nil)
-                return
-            }
-            
-            let symbols = Symbols(symbols: responseJSON.symbols)
-            callback(true, symbols)
         }
-        task.resume()
+        task?.resume()
     }
     
-    static func convert(from: String, to: String, amount: String, callback: @escaping (Bool, Convertion?) -> Void) {
+    func convert(from: String, to: String, amount: String, callback: @escaping (Bool, Convertion?) -> Void) {
         let url = "https://api.apilayer.com/fixer/convert?to=\(to)&from=\(from)&amount=\(amount)"
         var request = URLRequest(url: URL(string: url)!)
         
@@ -45,25 +53,29 @@ class CurrenciesService {
         request.addValue("TeOLN3KvPxA1VOD4az4SXQLi7ac7RE71", forHTTPHeaderField: "apikey")
         
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                callback(false, nil)
-                return
+        
+        task?.cancel()
+        task = session.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+                
+                guard let responseJSON = try? JSONDecoder().decode(Convertion.self, from: data) else {
+                    callback(false, nil)
+                    return
+                }
+                
+                let convertedAmount = Convertion(result: responseJSON.result)
+                callback(true, convertedAmount)
             }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                callback(false, nil)
-                return
-            }
-            
-            guard let responseJSON = try? JSONDecoder().decode(Convertion.self, from: data) else {
-                callback(false, nil)
-                return
-            }
-            
-            let convertedAmount = Convertion(result: responseJSON.result)
-            callback(true, convertedAmount)
         }
-        task.resume()
+        task?.resume()
     }
 }
