5e6cd95f9dd717ea20d8c29793e25f66bc231e7d
Thomas Ferré <t.ferre@digitsole.com>
2022-08-08 10:19:41 +0200

    [#Errors] API Errors
    - APIErrors enum created and implemented

---
+++ MonBaluchon/MonBaluchon/Errors/APIErrors.swift
@@ -0,0 +1,25 @@
+//
+//  APIErrors.swift
+//  MonBaluchon
+//
+//  Created by TomF on 08/08/2022.
+//
+
+import Foundation
+
+public enum APIErrors: Error {
+    case badURL
+    case badResquest
+    case dataParsing
+    
+    var description: String {
+        switch self {
+        case .badURL:
+            return "Bad URL used to perform this action"
+        case .badResquest:
+            return "Bad request to perform this action"
+        case .dataParsing:
+            return "Can't fetch datas"
+        }
+    }
+}
